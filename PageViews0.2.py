908bd4298683e23fa9a07057ec151e3f871a30fa
mhowe21 <48158483+mhowe21@users.noreply.github.com>
2020-06-05 12:02:19 -0600

    Add files via upload

---
+++ PageViews0.2.py
@@ -0,0 +1,122 @@
+# imports and install
+import json
+import multiprocessing
+import itertools
+import subprocess
+import sys
+try:
+    import requests
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+
+
+class run():
+    def run(self):
+        u = userInput()
+        c = calls()
+        env = u.canvasEnv()
+        tok = u.token()
+        uID = u.userIDs()
+        sd = u.startDate()
+        ed = u.endDate()
+
+        p = multiprocessing.Pool()
+        print("running...")
+        try:
+            p.starmap(c.pages, zip(uID, itertools.repeat(env), itertools.repeat(
+                tok), itertools.repeat(sd), itertools.repeat(ed)))
+            p.join()
+            p.close()
+            print("done!")
+        except multiprocessing.pool.MaybeEncodingError:
+            p.close()
+            print("done!")
+
+
+class calls():
+    def pages(self, userID, instance, token, startDate, endDate):
+
+        f = open("user " + userID + " pagesFile.csv", "a")
+        f.write("created_at"+ "," + "updated_at" + "," + "url" + "," + "participated" + "," + "http_method" + "," + "user_agent" + "," + "remote_ip"+ '\n')
+
+        url = "https://" + instance + ".instructure.com/api/v1/users/" + userID + "/page_views"
+        f = open("user " + userID + " pagesFile.csv", "a")
+        # format csv headers
+        f.write("created at" + "," + "updated at" + "," + "url" + "," + "participated" + "," + "http method" + "," + "user agent" + "," + "remote ip" + '\n')
+
+        payload = {'per_page': '100',
+                   'start_time': startDate,
+                   'end_time': endDate}
+
+        headers = {
+            'Authorization': 'Bearer ' + str(token)
+        }
+        while(url != None):
+
+            response = requests.request(
+                "GET", url, headers=headers, data=payload)
+
+            JSONResponse = response.json()
+            # print(JSONResponse)
+<<<<<<< HEAD
+=======
+            f = open("user " + userID + " pagesFile.csv", "a")
+>>>>>>> 83e1d233029fce0c453baf68e058b135aaca331c
+            
+
+            for i in range(len(JSONResponse)):
+                f.write(str(JSONResponse[i]["created_at"]) + "," + str(JSONResponse[i]["updated_at"]) + "," + str(JSONResponse[i]["url"]) + "," + str(JSONResponse[i]["participated"]) + "," + str(JSONResponse[i]["http_method"]) + "," + str(
+<<<<<<< HEAD
+                    JSONResponse[i]["user_agent"]).replace(",", "") +"," + str(JSONResponse[i]["remote_ip"]) + '\n')
+=======
+                    JSONResponse[i]["user_agent"]).replace(","," ") + "," + str(JSONResponse[i]["remote_ip"]) + '\n')
+>>>>>>> 83e1d233029fce0c453baf68e058b135aaca331c
+
+            # canvas paginates to results of 100 so we need to get the next relitivle link if there are more then 100 results
+            try:
+                rLinks = response.links['next']['url']
+                url = rLinks
+            except KeyError:
+                url = None
+            #rateLimit = response.headers['X-Rate-Limit-Remaining']
+
+        f.close()
+
+        return f
+
+
+class userInput():
+    def canvasEnv(self):
+        cEnv = input("enter canvas domain: ")
+        return cEnv
+
+    def token(self):
+        tok = input("enter your token: ")
+        return tok
+
+    def userIDs(self):
+        uID = input("enter the user id's seperated by comma: ")
+        uID = uID.replace(" ", "")
+        userList = uID.split(",")
+        return userList
+
+    def startDate(self):
+        startDate = input(
+            "enter the page view start date, leave blank for all. (yyyy-mm-dd): ")
+        startDate = startDate.replace(" ", "")
+        return startDate
+
+    def endDate(self):
+        endDate = input(
+            "enter the page view end date, leave blank for all. (yyyy-mm-dd): ")
+        endDate = endDate.replace(" ", "")
+        return endDate
+
+
+def main():
+    r = run()
+    r.run()
+
+
+if __name__ == "__main__":
+    main()
